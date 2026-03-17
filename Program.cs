93743ada9a5a8730ddd1afbed9b67373e872c195
mariacarolh <mcp.carolina2020@gmail.com>
2026-03-17 16:09:34 -0300

    Adicionando funcionalidade de vidas para o game e ajustando indentações

--- Program.cs
+++ Program.cs
@@ -4,16 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace jogo_forca
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace jogo_forca {
+    internal class Program {
+        static void Main(string[] args) {
             bool sair = false;
 
-            do
-            {
+            do {
                 Console.Clear();
                 Console.WriteLine("===== JOGO DA FORCA =====");
                 Console.WriteLine("1 - Jogar");
@@ -24,8 +20,7 @@ namespace jogo_forca
 
                 string opcao = Console.ReadLine();
 
-                switch (opcao)
-                {
+                switch (opcao) {
                     case "1":
                         Jogar();
                         break;
@@ -46,8 +41,7 @@ namespace jogo_forca
             } while (!sair);
         }
 
-        static void Jogar()
-        {
+        static void Jogar() {
             Console.Clear();
             Console.WriteLine("Bem-vindo ao jogo da forca!");
             Console.WriteLine();
@@ -59,25 +53,25 @@ namespace jogo_forca
             string[] letrasIdentificadas = new string[palavraEscolhida.Length]; // identifica a quantidade de letras da palavra escolhida e cria um array com a mesma quantidade de posições com o valor null
             int tamanhoPalavra = palavraEscolhida.Length;
             bool jogoEmAndamento = true;
+            int numeroVidas = 6;
 
-            do
-            {
+            do {
                 Console.Clear();
+                Console.WriteLine("Vidas restantes: " + numeroVidas);
+                Console.WriteLine();
+
                 Console.Write("A palavra escolhida é: ");
 
                 // percorre cada posição da palavra
-                for (int i = 0; i < letrasIdentificadas.Length; i++)
-                {
+                for (int i = 0; i < letrasIdentificadas.Length; i++) {
                     // pega a letra que foi descoberta na posição 
                     string letraAtual = letrasIdentificadas[i];
 
-                    if (letraAtual == null)
-                    {
+                    if (letraAtual == null) {
                         // exibe _ quando não encontrar a letra
                         Console.Write("_ ");
                     }
-                    else
-                    {
+                    else {
                         // exibe a letra se ela tiver sido encontrada
                         Console.Write(letraAtual);
                     }
@@ -93,42 +87,45 @@ namespace jogo_forca
                 bool letraEncontrada = false;
                 Console.WriteLine(letraEscolhida);
 
-                for (int i = 0; i < tamanhoPalavra; i++)
-                {
+                for (int i = 0; i < tamanhoPalavra; i++) {
                     string letraAtual = palavraEscolhida[i].ToString();
 
-                    if (letraAtual == letraEscolhida)
-                    {
+                    if (letraAtual == letraEscolhida) {
                         letrasIdentificadas[i] = letraAtual + " ";
                         letraEncontrada = true;
                     }
                 }
 
-                if (letraEncontrada == true)
-                {
+                if (letraEncontrada == true) {
                     bool palavraDescoberta = true;
-                    for (int i = 0; i < letrasIdentificadas.Length; i++)
-                    {
+                    for (int i = 0; i < letrasIdentificadas.Length; i++) {
                         string letraAtual = letrasIdentificadas[i];
 
-                        if (letraAtual == null)
-                        {
+                        if (letraAtual == null) {
                             palavraDescoberta = false;
                         }
                     }
 
-                    if (palavraDescoberta == true)
-                    {
+                    if (palavraDescoberta == true) {
                         Console.WriteLine();
                         Console.WriteLine("Parabéns, você ganhou! A palavra secreta era " + palavraEscolhida + ".");
                         jogoEmAndamento = false;
                     }
                 }
-                else
-                {
+                else {
+                    numeroVidas--;
                     Console.WriteLine("A letra " + letraEscolhida + " não pertence a PALAVRA SECRETA!!!");
-                    Console.WriteLine("Pressione qualquer tecla");
+                    Console.WriteLine();
+                    Console.WriteLine("Vidas restantes: " + numeroVidas);
+                    Console.WriteLine();
+                    Console.WriteLine("Pressione qualquer tecla para continuar");
                     Console.ReadKey();
+
+                    if (numeroVidas == 0) {
+                        Console.WriteLine();
+                        Console.WriteLine("Você perdeu! A palavra era: " + palavraEscolhida);
+                        jogoEmAndamento = false;
+                    }
                 }
             } while (jogoEmAndamento);
 
